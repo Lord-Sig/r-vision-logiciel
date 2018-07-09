f26e112b7b7664e78e06b1434e74069c23388040
Theodore Semertzidis <theodore.semertzidisallais@gmail.com>
2018-07-09 12:38:52 +0200

    first commit

---
+++ src/trucpourrev.cpp
@@ -0,0 +1,79 @@
+/*
+ * trucpourrev.cpp
+ *
+ *  Created on: 7 juil. 2018
+ *      Author: totod
+ */
+
+#include "trucpourrev.h"
+#include <fstream>;
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ *
+ */
+int main()
+{
+	cout << "Bonjours est bienvenue dans mon programme de révision"<<"\n"<<"Nous rappelons que le programme n est pas fournit avec les question"<< endl; // ligne du message d'acueil
+	choixaction();
+	return 0;
+}
+void choixaction(){
+	int entreuser = 0;
+	cout <<"Menu des action"<<"\n"<< "Ecrit 1 pour ajouté une question/reponse" <<"\n"<<"Ecrit 2 pour lancé un exercice"<<"\n"<<endl;// cette ligne annonce les diférentes action du menu
+	cin >> entreuser;
+	if (entreuser == 1){
+		entreuser = 0;
+
+		writequestionanswer();
+	}
+	else if (entreuser == 2){
+		entreuser = 0;
+
+	}
+
+}
+/*
+void exodequestion(){
+	string question;
+	string reponse;
+	vector<string> questionL();
+	vector<string> reponseL();
+	ifstream questionFlux("question.txt");  //Ouverture d'un fichier en lecture
+	ifstream reponseFlux("reponse.txt");
+
+	if(questionFlux )
+	{
+		while(getline(questionFlux,question)) //Tant qu'on n'est pas à la fin, on lit
+		      {
+				getline(reponseFlux,reponse);
+				questionL.push_back(question);
+
+		      }
+
+	}
+	else
+	{
+	    cout << "ERREUR: Impossible d'ouvrir le fichier en lecture  ou fichier inexistant" << endl;
+	}
+}*/
+/**
+ *
+ */
+void writequestionanswer()
+{
+	string question;
+	string reponse;
+		cout << "entré la question"<<endl;
+		cin >> question;
+		cout <<"entré la réponse" << endl;
+		cin >> reponse;
+	ofstream questionFlux("question.txt", ios::app); // on ouvre le ficher d'écriture
+	ofstream reponseFlux("reponse.txt", ios::app);
+	questionFlux << question << endl;
+	reponseFlux << reponse <<endl;
+	choixaction();
+
+}
---
+++ src/trucpourrev.h
@@ -0,0 +1,16 @@
+/*
+ * trucpourrev.h
+ *
+ *  Created on: 7 juil. 2018
+ *      Author: totod
+ */
+
+#ifndef TRUCPOURREV_H_
+#define TRUCPOURREV_H_
+#include <string>
+using namespace std;
+ void writequestionanswer();
+ void choixaction();
+
+
+#endif /* TRUCPOURREV_H_ */
